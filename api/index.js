0652bf3f1f32dff8000ab0deccd53f4a4c831880
Luciano Silva <lcnsilvajf@gmail.com>
2024-10-31 12:00:52 -0300

    feat: add render config

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import dotenvx from '@dotenvx/dotenvx';
+import 'dotenv/config'
 import dbConnection from './config/dbConnection.js'
 
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 const result = await dbConnection();
-dotenvx.config();
+
 app.get('/', (req,res) => {
     res.send('teste' + result);
 })
 
-app.listen(3000, (req,res) => {
-    console.log('Rodando na porta 3000.')
+app.listen(PORT, (req,res) => {
+    console.log(`Rodando na porta ${PORT}`)
 });
